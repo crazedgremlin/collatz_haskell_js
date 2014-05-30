f5209f986673f2e43b5e829f84269d384c3c650c
Dan McArdle <zingermc@gmail.com>
2014-05-29 21:34:09 -0400

    Working, but exceeds maximum recursion depth quite easily

--- collatz.hs
+++ collatz.hs
@@ -1,10 +1,9 @@
+module Main where
 import Haste
-import Haste.Graphics.Canvas
 import Haste.App
+import Haste.Graphics.Canvas
 
-{-
- - Compute the stopping time for the input n, where n is an integer and n > 0.
- -}
+-- Compute the stopping time for the input n, where n is an integer and n > 0.
 collatzCount :: Int -> Int
 collatzCount n = length $ takeWhile (>1) $ iterate collatz n
   where
@@ -13,24 +12,53 @@ collatzCount n = length $ takeWhile (>1) $ iterate collatz n
       | mod m 2 == 0    =   div m 2
       | otherwise       =   3*m + 1
 
+-- For the given x value, create the Point that should be plotted.
+collatzPoint :: Int -> Int -> Point
+collatzPoint x yMax = (fromIntegral x, yMax' - yScale*stoppingTime)
+  where
+    n :: Int
+    n = round ((fromIntegral x) * xScale)
 
+    yMax' :: Double
+    yMax' = fromIntegral yMax
+
+    stoppingTime :: Double
+    stoppingTime = fromIntegral $ collatzCount n
+
+-- Draws a point to the canvas without clearing.
 drawPoint :: MonadIO m => Canvas -> Point -> m ()
-drawPoint c (x,y) = renderOnTop c $ fill $ rect (x,y) (x+1,y+1)
+drawPoint c (x,y) = renderOnTop c $ pixel
+  where
+    pixel :: Picture ()
+    pixel = color (RGBA 0xFF 0x00 0x00 0.3) $ fill $ circle (x,y) 1.0
 
-createPoint :: (Int,Int) -> Point
-createPoint (a,b) = (fromIntegral a, fromIntegral b)
+drawTickX :: MonadIO m => Canvas -> Int -> m ()
+drawTickX c x = renderOnTop c $ text (fromIntegral x, 10) $ show x
 
-{- 
- - Plot the stopping times of the values along the x axis.
- -}
+
+
+xScale, yScale :: Double
+xScale = 1/15
+yScale = 3
+
+-- Plot the stopping times of the values along the x axis.
 main :: IO ()
 main = do
   Just ce <- elemById "canvas"
   Just c <- getCanvas ce
 
-  -- render c $ text (110, 120) "Loading, please wait..."
-  --render c $ fill $ rect (20, 20) (21, 21)
+  --setStyle ce "background-color" "black"
 
-  mapM (\x -> drawPoint c (createPoint (x, collatzCount x))) [0..639]
+  widthStr <- getAttr ce "width"
+  heightStr <- getAttr ce "height"
+  let Just width = fromString widthStr
+  let Just height = fromString heightStr
+  writeLog widthStr
+  writeLog heightStr
+
+  let nLast :: Int
+      nLast = round (width/xScale) - 1
+  mapM (\x -> drawPoint c (collatzPoint x height)) [0..fromIntegral nLast]
+  mapM (\x -> drawTickX c x) [0,100..nLast] 
 
   return ()
