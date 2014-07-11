f11bd4d3730a0316f5016034cbd7f57fd4abb7df
Dan McArdle <zingermc@gmail.com>
2014-07-10 20:06:34 -0400

    Fixed stack overflow bug with mapM_, which executes only for side-effects

--- collatz.hs
+++ collatz.hs
@@ -17,7 +17,7 @@ collatzPoint :: Int -> Int -> Point
 collatzPoint x yMax = (fromIntegral x, yMax' - yScale*stoppingTime)
   where
     n :: Int
-    n = round ((fromIntegral x) * xScale)
+    n = round ((fromIntegral x) / xScale)
 
     yMax' :: Double
     yMax' = fromIntegral yMax
@@ -25,21 +25,10 @@ collatzPoint x yMax = (fromIntegral x, yMax' - yScale*stoppingTime)
     stoppingTime :: Double
     stoppingTime = fromIntegral $ collatzCount n
 
--- Draws a point to the canvas without clearing.
-drawPoint :: MonadIO m => Canvas -> Point -> m ()
-drawPoint c (x,y) = renderOnTop c $ pixel
-  where
-    pixel :: Picture ()
-    pixel = color (RGBA 0xFF 0x00 0x00 0.3) $ fill $ circle (x,y) 1.0
-
-drawTickX :: MonadIO m => Canvas -> Int -> m ()
-drawTickX c x = renderOnTop c $ text (fromIntegral x, 10) $ show x
-
-
 
 xScale, yScale :: Double
-xScale = 1/15
-yScale = 3
+xScale = 1/12 
+yScale = 2
 
 -- Plot the stopping times of the values along the x axis.
 main :: IO ()
@@ -52,13 +41,24 @@ main = do
   widthStr <- getAttr ce "width"
   heightStr <- getAttr ce "height"
   let Just width = fromString widthStr
-  let Just height = fromString heightStr
-  writeLog widthStr
-  writeLog heightStr
+      Just height = fromString heightStr
 
   let nLast :: Int
       nLast = round (width/xScale) - 1
-  mapM (\x -> drawPoint c (collatzPoint x height)) [0..fromIntegral nLast]
-  mapM (\x -> drawTickX c x) [0,100..nLast] 
+
+  --writeLog $ show nLast
+
+  let drawTickX :: MonadIO m => Int -> m ()
+      drawTickX x = renderOnTop c $ text (fromIntegral x * xScale, 10) $ show x
+
+      -- Draws a point to the canvas without clearing.
+      drawPoint :: MonadIO m => Point -> m ()
+      drawPoint (x,y) = renderOnTop c $ pixel
+        where
+          pixel :: Picture ()
+          pixel = color (RGBA 0xFF 0x00 0x00 0.8) $ fill $ circle (x,y) 1.0
+
+  mapM_ (\x -> drawPoint (collatzPoint x height)) [0..fromIntegral nLast]
+  mapM_ (\x -> drawTickX x) [0,500..nLast] 
 
   return ()
